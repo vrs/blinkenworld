a806328b47113394d16d74ddc7106b61ef176730
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-11-16 00:35:39 +0100

    + simple openlayers integration

--- main.js
+++ main.js
@@ -1,127 +1,60 @@
-var debug = true, log;
-if (typeof console !== 'undefined' && debug)
-	log = (function (x) {console.log(x)});
-else
-	log = function () {};
+var map = new OpenLayers.Map(
+    'map',
+    {maxResolution: 0.703125}
+);
 
-document.addEventListener('DOMContentLoaded',function(){
-	var intervalMs = 50,
-	secondsPerInterval = 60,
+var wmscURL = [
+    "http://wmsc1.terrapages.net/getmap?",
+    "http://wmsc2.terrapages.net/getmap?",
+    "http://wmsc3.terrapages.net/getmap?",
+    "http://wmsc4.terrapages.net/getmap?"
+];
+var terrapagesStreetLayer = new OpenLayers.Layer.WMS(
+    'TerraPages Street',
+    wmscURL,
+    {
+        layers: 'UnprojectedStreet',
+        format: 'image/jpeg'
+    },
+    {
+        buffer: 1,
+        isBaseLayer: true
+    }
+);
+map.addLayer(terrapagesStreetLayer);
 
-	prepareCanvas = function prepareCanvas(loadEvent) {
-		var background = loadEvent.target;
-		canvas.width = background.width;
-		canvas.height = background.height;
-	},
-	processPosts = (function processPosts(response) {
-		if (response.status !== 200 && response.status !== 304) {
-			log("oops, HTTP " + response.status);
-			return
-		} else {
-			log("found, HTTP " + response.status)
-		}
-		return JSON.parse(response.text)
-	})
-	.then(function animatePosts(data) {
-		log("animating...");
-		var width = canvas.width,
-		height = canvas.height;
-		
-		ctx.fillStyle = '#FFFFFF';
-		data.posts.forEach(function(post){
-			animatePost(
-				(180+post.longitude)%360*width/360,
-				(90-post.latitude)*height/180);
-		});
-		// animationQueue
-		// zeittabelle
-		// tabelle durchlaufen (setInterval), an animationQueue anhängen
-		// gleichzeitig animationQueue abarbeiten, setTimeout
-		// oder einfach losanimieren
-	})
-	.twice(),
-	animatePost = function animatePost(longitudepx, latitudepx) {
-		ctx.beginPath();
-		ctx.arc(longitudepx, latitudepx, 10, 0, Math.PI*2, true); 
-		ctx.closePath();
-		ctx.fill();
-		// fade out
-		// destroy
-	},
-	
-	canvas = document.getElementById('vis'),
-	ctx = canvas.getContext('2d');
+map.setCenter(new OpenLayers.LonLat(12, 50), 0);
+map.zoomTo(3);
 
-	// go
-	load('worldmap.png').then(prepareCanvas).then(processPosts).run();
-	load(debug ? 'postdata-sample.json' : 'postdata.json').then(processPosts).run();
-	// then(reset)?
+document.addEventListener('DOMContentLoaded', function (){
+    var markersLayer = new OpenLayers.Layer.Markers('Countryballs');
+    var iconSize =  new OpenLayers.Size(17, 14);
+    var iconOffset = new OpenLayers.Pixel(-(iconSize.w/2), -iconSize.h);
+
+    var Austria     = [16, 48, 'at.png'];
+    var Bavaria     = [11, 48, 'bavaria.png'];
+    var Denmark     = [10, 56, 'dk.png'];
+    var France      = [ 3, 46, 'fr.png'];
+    var Germany     = [12, 52, 'de.png'];
+    var Netherlands = [ 6, 52, 'nl.png'];
+    var Poland      = [21, 52, 'pl.png'];
+    var Switzerland = [ 8, 46, 'ch.png'];
+    
+    var Countryballs = [Austria, Bavaria, Denmark, France, Germany, Netherlands, Poland, Switzerland];
+    
+    for (i in Countryballs) {
+        var lon = Countryballs[i][0];
+        var lat = Countryballs[i][1];
+        var iconURL = 'img/countryballs/' + Countryballs[i][2];
+    
+        var marker = new OpenLayers.Marker(
+            new OpenLayers.LonLat(lon, lat),
+            new OpenLayers.Icon(iconURL, iconSize, iconOffset)
+        );
+        
+        markersLayer.addMarker(marker);
+    };
+
+    map.addLayer(markersLayer);
+    markersLayer.setVisibility(true);
 }, false);
-
-/* 
- * library functions
- * inspired by Arrowlets
- * http://www.cs.umd.edu/projects/PL/arrowlets/
- */
-// asynchronous loading of content, starting a pipe
-function load(path) {
-	function loadImage(callback) {
-		var img = document.createElement('img');
-		img.addEventListener('load', callback, false);
-		img.src = path;
-	}
-	function loadResource(callback) {
-		var req = new XMLHttpRequest();
-		req.open('GET', path, true);
-		req.onreadystatechange = function () {
-			if (req.readyState === 4) {
-				callback({
-					text: req.responseText,
-					status: req.status
-				})
-			}
-		};
-		req.send(null);
-	}
-	return ({ then: function (callback) {
-		return new Async(callback, /\.(png|jpg|gif)$/.test(path) ? loadImage : loadResource)
-	}})
-}
-// asynchronous pipe
-function Async (fun, hook) {
-	this.fun = fun;
-	this.hook = hook;
-}
-Async.prototype.then = function (fun) {
-	// some crocheting
-	return new Async(Function.prototype.then.call(this.fun, fun), this.hook)
-}
-Async.prototype.run = function () {
-	return this.hook(this.fun);
-}
-
-// synchronous pipe
-Function.prototype.then = function (g) {
-	var f = this;
-	return function () {
-		var x = Array.prototype.slice.apply(arguments);
-		return g(f.apply(f, x))
-	}
-}
-// defer the first function call, wait for the next one
-Function.prototype.twice = function () {
-	var alreadycalled = 0,
-	args = [],
-	f = this;
-	return function() {
-		// accumulate the arguments
-		args = Array.prototype.concat.apply(args, arguments).filter(function (x) {
-				return typeof x !== 'undefined'
-		});
-		if (++alreadycalled < 2) return;
-		else return f.apply(f, args);
-	}
-}
-Function.prototype.run = function () { // f.run() alias for f()
-	return this.apply(this, Array.prototype.slice.apply(arguments));
-}
